f573d07bcecaa3dc3bec3ecacacee6eba2c54c4b
Patricio104 <patriciojuarez104@gmail.com>
2021-07-20 23:53:44 -0500

    Control flow for

--- app/src/main/java/com/patricio/aprendiendokotlin/Main.kt
+++ app/src/main/java/com/patricio/aprendiendokotlin/Main.kt
@@ -3,22 +3,28 @@ package com.patricio.aprendiendokotlin
 
 fun main(){
 
-    val a=3
-    val b = 2
-    //var max = a
-    //if (a<b) max =b
-    //println(max)
+    //Lista mutable, pueder ser modificada
+    var listaMutable = mutableListOf<String>("Patricio", "Gonzalo", "Benigno", "Santos")
+    //Lista inmutable, no puede ser modificada
+    var listaInmutable = listOf<String>("Bici", "Moto", "Auto")
 
-    /*var max: Int
-    if (a > b){
-        max =a
-    }else{
-        max= b
-    }*/
+    for (nombre in listaMutable){
+        println(nombre)
+    }
 
-    val max = if (a > b) a else b
 
-    println(max)
+    for((index, value) in listaMutable.withIndex()){
+        println("Nombre : $value con indice: $index")
+    }
+
+    listaInmutable.forEach { elementos ->
+        println(elementos)
+    }
+
+    listaInmutable.forEachIndexed { index, elemento ->
+        println("Elemento : $elemento con indice: $index")
+    }
+
 }
 
 
