c0c58022adf2de678dfbafe5eb612d56f2b87e00
Patricio104 <patriciojuarez104@gmail.com>
2021-07-21 00:28:29 -0500

    Nullabilidad

--- app/src/main/java/com/patricio/aprendiendokotlin/Main.kt
+++ app/src/main/java/com/patricio/aprendiendokotlin/Main.kt
@@ -1,17 +1,37 @@
 package com.patricio.aprendiendokotlin
 
 
-fun main(){
+fun main() {
 
-    //Extension functions
-    val nombre = "Patricio"
-    println(nombre.toLowerCase())
+    //Nullabilidad
+    // ? indicar que puede ser nulo
+    // !! asegurarse de que no es nulo (no recomendado)
 
-    println("Remover primer caracter: ${nombre.removerPrimerYUltimoCaracter()}")
+    val nombre: String = "Patricio"
+    println(nombre.length)
+
+    //imprime null
+
+    val nombre2: String? = null
+    println(nombre2?.length)
+
+    //Provoca el típico Exception in thread "main" java.lang.NullPointerException
+    //val nombre3: String? = null
+    //println(nombre3!!.length)
+
+
+    //Cuando nuestra variable sea nulo se puede notificar retornando otro valor que no sea null
+    val nombre4: String? = null
+    val c = nombre4?.length ?: "Patrick".length
+    println(c)
+
+    //Usando let, solamente va entrar cuando nuestra variable no sea nula
+    val nombre5: String? = "Patricio"
+    nombre5?.let {
+        println(it)
+    }
 
 }
-
-fun String.removerPrimerYUltimoCaracter(): String = this.substring(1, this.length-1)
 
 
 
