7a9c913e810cd6873b02c6e195d1457cd981cfa8
Patricio104 <patriciojuarez104@gmail.com>
2021-07-20 22:59:42 -0500

    Funciones

--- app/src/main/java/com/patricio/aprendiendokotlin/Main.kt
+++ app/src/main/java/com/patricio/aprendiendokotlin/Main.kt
@@ -2,17 +2,10 @@ package com.patricio.aprendiendokotlin
 
 
 fun main(){
-    val dia = DIAS.LUNES.numero
-    println(dia)
+    println(esPar(3))
 }
 
-enum class DIAS(val numero: Int) {
-    LUNES(1),
-    MARTES(2),
-    MIERCOLES(3),
-    JUEVES(4),
-    VIERNES(5),
-    SABADO(6),
-    DOMINGO(7)
+fun esPar(numero: Int): Boolean {
+    return numero % 2 == 0
 }
 
